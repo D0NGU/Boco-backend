5f362d9d1ec7a4b711ffa9db2892dd30d7fa2368
matssg <matssg@stud.ntnu.no>
2022-04-20 09:23:15 +0200

    Initial commit

---
+++ src/main/java/ntnu/idatt/boco/BoCoServerApplication.java
@@ -0,0 +1,13 @@
+package ntnu.idatt.boco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoCoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoCoServerApplication.class, args);
+	}
+
+}
