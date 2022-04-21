a9bb4f83eab8deb37acbc863fb02d0deb88ffe1e
joelmt <joelmt@stud.ntnu.no>
2022-04-21 10:33:04 +0200

    Methods for retrieving rentals by product_id

--- src/main/java/ntnu/idatt/boco/controller/RentalController.java
+++ src/main/java/ntnu/idatt/boco/controller/RentalController.java
@@ -10,15 +10,34 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/rentals")
 public class RentalController {
     Logger logger = LoggerFactory.getLogger(AuthController.class);
     @Autowired
     RentalRepository rentalRepository;
 
-    @PostMapping("/rental")
+    @GetMapping("/product/{id}")
+    public ResponseEntity<List<Rental>> getRentals(@PathVariable String id) {
+        logger.info("New GET-request for rentals with product_id " + id);
+        try {
+            List<Rental> resultList = rentalRepository.getRentals(id);
+            if (resultList.isEmpty()) {
+                logger.info("No rentals with product_id " + id + " found");
+                return new ResponseEntity<>(null, HttpStatus.OK);
+            }
+            logger.info("Success - rentals retrieved");
+            return new ResponseEntity<>(resultList, HttpStatus.OK);
+        } catch(Exception e) {
+            logger.error("Rental retrieval error", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/")
     public ResponseEntity<String> registerNewRental(@RequestBody Rental rental) {
         logger.info("New rental registration requested");
         try {
--- src/main/java/ntnu/idatt/boco/repository/RentalRepository.java
+++ src/main/java/ntnu/idatt/boco/repository/RentalRepository.java
@@ -2,13 +2,20 @@ package ntnu.idatt.boco.repository;
 
 import ntnu.idatt.boco.model.Rental;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class RentalRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    public List<Rental> getRentals(String productId) {
+        return jdbcTemplate.query("SELECT * FROM rentals WHERE product_id = ?;", BeanPropertyRowMapper.newInstance(Rental.class), productId);
+    }
 
     public int saveRentalToDatabase(Rental rental) {
         return jdbcTemplate.update("INSERT INTO rentals (date_from, date_to, product_id, user_id) VALUES (?,?,?,?);",
