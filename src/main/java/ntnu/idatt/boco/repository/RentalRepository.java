03308e5fa2f3fa4d8aeca93869b3176c22878622
joelmt <joelmt@stud.ntnu.no>
2022-04-21 13:19:10 +0200

    Javadoc for "RentalController.java" and "RentalRepository.java"

--- src/main/java/ntnu/idatt/boco/repository/RentalRepository.java
+++ src/main/java/ntnu/idatt/boco/repository/RentalRepository.java
@@ -8,15 +8,28 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * This class contains methods relating to registering and retrieving rentals to/from the database.
+ */
 @Repository
 public class RentalRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    /**
+     * Returns a list of all rentals with a certain product_id.
+     * @param productId the product_id of the rentals to be retrieved
+     * @return a list containing all rentals with the correct product_id
+     */
     public List<Rental> getRentals(String productId) {
         return jdbcTemplate.query("SELECT * FROM rentals WHERE product_id = ?;", BeanPropertyRowMapper.newInstance(Rental.class), productId);
     }
 
+    /**
+     * Method for saving a new rental object to the database.
+     * @param rental the rental object to be saved to the database
+     * @return the number of rows in the database that was affected by the SQL insertion
+     */
     public int saveRentalToDatabase(Rental rental) {
         return jdbcTemplate.update("INSERT INTO rentals (date_from, date_to, product_id, user_id) VALUES (?,?,?,?);",
                 new Object[] { rental.getDateFrom(), rental.getDateTo(), rental.getProductId(), rental.getUserId()});
