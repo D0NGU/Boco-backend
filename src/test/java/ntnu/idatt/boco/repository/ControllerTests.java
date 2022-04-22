f7780e19a396c772e09d63be416cd8008208a023
Dominykas Mazys <dominykasmazys@gmail.com>
2022-04-22 14:02:28 +0200

    added tests for RentalController

--- src/test/java/ntnu/idatt/boco/repository/ControllerTests.java
+++ src/test/java/ntnu/idatt/boco/repository/ControllerTests.java
@@ -20,6 +20,9 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+/**
+ * This test class contains test-methods for the methods in the controllers
+ */
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ActiveProfiles("test") // Makes the tests use the test database instead of the normal one
 @SpringBootTest
@@ -71,7 +74,7 @@ public class ControllerTests {
     @Order(5)
     public void successfullyRegisteredNewProduct() {
         Product product = new Product(1, "Hammer", "Description", "Address", 20.0,
-                false, new Date(2022, 5, 12), new Date(2022, 5, 16), 1, "verktøy");
+                false, new Date(122, 5, 12), new Date(122, 5, 16), 1, "verktøy");
         assertEquals("Created successfully!", productController.newProduct(product).getBody());
     }
 
@@ -79,7 +82,7 @@ public class ControllerTests {
     @Order(6)
     public void successfullyRetrievedAllProducts() {
         Product product = new Product(1, "Hammer", "Description", "Address", 20.0,
-                false, new Date(2022, 5, 12), new Date(2022, 5, 16), 1, "verktøy");
+                false, new Date(122, 5, 12), new Date(122, 5, 16), 1, "verktøy");
         List<Product> list = new ArrayList<>();
         list.add(product);
         assertEquals(list.toString(), productController.getAll().getBody().toString());
@@ -89,7 +92,7 @@ public class ControllerTests {
     @Order(7)
     public void successfullyEditedProduct(){
         Product product = new Product(1, "Hammer", "Something else", "New address", 200.0,
-                false, new Date(2022, 5, 12), new Date(2022, 5, 16), 1, "verktøy");
+                false, new Date(122, 5, 12), new Date(122, 5, 16), 1, "verktøy");
         assertEquals("Created successfully!",productController.editProduct("1", product).getBody());
     }
 
@@ -97,7 +100,7 @@ public class ControllerTests {
     @Order(8)
     public void successfullyRetrievedProductById() {
         Product product = new Product(1, "Hammer", "Something else", "New address", 200.0,
-                false, new Date(2022, 5, 12), new Date(2022, 5, 16), 1, "verktøy");
+                false, new Date(122, 5, 12), new Date(122, 5, 16), 1, "verktøy");
         assertEquals(product.toString(), productController.getById("1").getBody().toString());
     }
 
@@ -105,10 +108,32 @@ public class ControllerTests {
     @Order(9)
     public void successfullyRetrievedProductsByCategory() {
         Product product = new Product(1, "Hammer", "Something else", "New address", 200.0,
-                false, new Date(2022, 5, 12), new Date(2022, 5, 16), 1, "verktøy");
+                false, new Date(122, 5, 12), new Date(122, 5, 16), 1, "verktøy");
         List<Product> list = new ArrayList<>();
         list.add(product);
         assertEquals(list.toString(), productController.getByCategory("verktøy").getBody().toString());
+    }
+
+    @Test
+    @Order(10)
+    public void successfullyRegisteredNewRental() {
+        Rental rental = new Rental(1, new Date(122, 5, 12), new Date(122, 5, 13), 1, 1);
+        assertEquals("Registered successfully!", rentalController.registerNewRental(rental).getBody());
+    }
+
+    @Test
+    @Order(11)
+    public void successfullyRetrievedRentalsByProductId() {
+        Rental rental = new Rental(1, new Date(122, 5, 12), new Date(122, 5, 13), 1, 1);
+        List<Rental> list = new ArrayList<>();
+        list.add(rental);
+        assertEquals(list.toString(), rentalController.getRentals("1").getBody().toString());
+    }
+
+    @Test
+    @Order(12)
+    public void successfullyRetrievedRentalsByProductIdWhenEmpty() {
+        assertNull(rentalController.getRentals("2").getBody());
     }
 
 
