7b300537c7596b6064dca27b031eed5ea4d7c399
matssg <matssg@stud.ntnu.no>
2022-04-22 12:22:38 +0200

    Add more temporary test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,24 @@
 INSERT INTO categories(category) VALUES 
-    ('verktøy'), 
-    ('sportsutstyr');
+    ('elektronikk'),
+    ('utstyr'),
+    ('klær'),
+    ('kjøretøy');
 
 INSERT INTO categories(CATEGORY, MAIN_CATEGORY) VALUES 
+    ('data', 'elektronikk'),
+    ('foto og video', 'elektronikk'),
+    ('hvitevarer', 'elektronikk'),
+    ('verktøy', 'utstyr'),
     ('skrutrekkere', 'verktøy'), 
-    ('hammere', 'verktøy'), 
-    ('stjerneskrutrekker', 'skrutrekkere');
+    ('hammere', 'verktøy'),
+    ('dyreutstyr', 'utstyr'),
+    ('hageutstyr', 'utstyr'),
+    ('bil', 'kjøretøy'),
+    ('båt', 'kjøretøy');
 
 INSERT INTO users (fname, lname, password, email, salt) 
-    VALUES ('test','tester',CAST('08dab1fea88143614b4d449ba5ec067d' AS VARBINARY(64)),'t.est@tset.edu',CAST('9eb8eb1886c3184fa3f3d963c1578f40' AS VARBINARY(64)));
+    VALUES ('test','tester',CAST('08dab1fea88143614b4d449ba5ec067d' AS VARBINARY(64)),'t.est@tset.edu',CAST('9eb8eb1886c3184fa3f3d963c1578f40' AS VARBINARY(64))); --password=test123
+
+INSERT INTO products(title, description, address, price, unlisted, user_id, category) 
+    VALUES ('John Deere 7280R', 'Pent brukt traktor!', 'Myrangvegen 4, 2040 Kløfta', 450, false, 1, 'kjøretøy'),
+    ('Valtra 34CX', 'Meget pent brukt traktor!!', 'Myrangvegen 6, 2040 Kløfta', 200, false, 1, 'kjøretøy');
