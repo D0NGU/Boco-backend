1f1b57e879689ddefc14a2ac8ac507593e4c37de
joelmt <joelmt@stud.ntnu.no>
2022-04-27 10:56:37 +0200

    Added multiple negative tests for methods in ProductController, RentalController and UserController

--- src/test/java/ntnu/idatt/boco/controller/UserControllerTest.java
+++ src/test/java/ntnu/idatt/boco/controller/UserControllerTest.java
@@ -35,7 +35,20 @@ public class UserControllerTest {
     }
 
     @Test
+    @Order(3)
+    public void unSuccessfullyEditedUserPasswordWithInvalidUserData() {
+        EditUserRequest request = new EditUserRequest(null, null, null);
+        assertEquals(500, userController.editPassword(request).getStatusCodeValue());
+    }
+
+    @Test
     @Order(4)
+    public void unSuccessfullyGetUserById() {
+        assertEquals(500, userController.getUserByUserId(36).getStatusCodeValue());
+    }
+
+    @Test
+    @Order(5)
     public void successfullyDeletedUser() {
         User user = new User();
         user.setEmail("t.est@tset.edu");
@@ -44,5 +57,17 @@ public class UserControllerTest {
         user.setPassword("123test");
 
         assertEquals(200, userController.deleteUser(user).getStatusCodeValue());
+    }
+
+    @Test
+    @Order(6)
+    public void unSuccessfullyDeletedUser() {
+        User user = new User();
+        user.setEmail(null);
+        user.setFname("test");
+        user.setLname("tester");
+        user.setPassword("123test");
+
+        assertEquals(500, userController.deleteUser(user).getStatusCodeValue());
     }
 }
