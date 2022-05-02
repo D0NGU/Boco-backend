437fdcca821e472690fc63c176ca57d500cdb927
krihaan <krihaan@stud.ntnu.no>
2022-05-02 15:04:42 +0200

    Searching for users that doesnt exist throws 404

--- src/main/java/ntnu/idatt/boco/controller/UserController.java
+++ src/main/java/ntnu/idatt/boco/controller/UserController.java
@@ -14,6 +14,7 @@ import ntnu.idatt.boco.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCrypt;
@@ -128,7 +129,16 @@ public class UserController {
     @GetMapping("/user/get/")
     public ResponseEntity<User> getUserById(@RequestParam int userId){
         logger.info("Getting user by id: {}", userId);
-        return new ResponseEntity<>(userService.getUserById(userId), HttpStatus.OK);
+        try {
+            return new ResponseEntity<>(userService.getUserById(userId), HttpStatus.OK);
+        }catch (EmptyResultDataAccessException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch (Exception e){
+                e.printStackTrace();
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("/user/{userid}/description")
