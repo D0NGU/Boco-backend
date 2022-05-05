f0f8d7169631e338dc554a8250d03e02b4ab285c
Mathangi Pushparajah <mathangp@stud.ntnu.no>
2022-05-05 09:42:51 +0200

    took away logging message of the product in editProduct

--- src/main/java/ntnu/idatt/boco/controller/ProductController.java
+++ src/main/java/ntnu/idatt/boco/controller/ProductController.java
@@ -66,7 +66,6 @@ public class ProductController {
     @PutMapping("/{productId}")
     public ResponseEntity<String> editProduct(@PathVariable int productId, @RequestBody Product product) {
         logger.info("Editing product: " + productId);
-        logger.info("Editing product: " + product.getAvailableFrom());
         try {
             productRepository.editProduct(product, productId);
             imageRepository.deleteProductsImages(productId);
